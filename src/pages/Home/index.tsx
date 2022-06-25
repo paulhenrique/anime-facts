ae3fc446e4a3d07ebc4d6a36a7eda2de2c1ccea9
Paulo Henrique <phvcandido@gmail.com>
2022-06-25 09:44:51 -0300

    Adicionando mui

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,10 @@
+import { Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+
 const Home = () => {
-  return <div>Home</div>;
+  const animeStatus = useSelector((state: RootState) => state.anime.status);
+  return <Typography>{animeStatus}</Typography>;
 };
 
 export default Home;
